b064022e0cdaf1d807ec25a632b08ced9178be76
Hisha <suyaehdz@gmail.com>
2026-09-24 00:02:11 -0600

    Agrega burbuja de WhatsApp y seguridad, mas? si

--- servidor.js
+++ servidor.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const aplicacion = express();
 const db = require('./config/db'); 
@@ -6,11 +7,18 @@ const sequelize = require('./config/db');
 const Producto = require('./models/Producto');
 const Pedido = require('./models/Pedido'); // <-- ¡Agrega esta línea!
 const cookieParser = require('cookie-parser');
+    
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
 
 // Middlewares
 aplicacion.use(express.urlencoded({ extended: false }));
 aplicacion.use(express.json());
 aplicacion.use(express.static(__dirname + '/public'));
+aplicacion.use(limiter);
 aplicacion.use(cookieParser());
 
 // Motor de plantillas
@@ -20,7 +28,6 @@ aplicacion.set('views', __dirname + '/views');
 const puerto = process.env.PORT || 3000;
 
 db.authenticate()
-    db.authenticate()
     .then(() => {
         console.log('Conectado a MySQL exitosamente.');
         return db.sync({ alter: true }); // <--- CAMBIO AQUÍ
